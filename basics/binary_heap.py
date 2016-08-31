d333400a3f726d849be614e6251dc6912643952f
Dan Puttick <dputtick@users.noreply.github.com>
2016-08-31 19:34:54 -0400

    first binary heap commit

---
+++ basics/binary_heap.py
@@ -0,0 +1,81 @@
+class BinaryHeap:
+    def __init__(self, inputlist=None):
+        self.heap_list = [0]
+        self.current_size = 0
+        if inputlist:
+            self.build_heap(inputlist)
+
+    def insert(self, value):
+        self.heap_list.append(value)
+        self.current_size += 1
+        self._perc_up(self.current_size)
+
+    def _perc_up(self, curr_index):
+        parent_index = curr_index // 2
+        while parent_index > 0:
+            if self.heap_list[curr_index] < self.heap_list[parent_index]:
+                self.heap_list[curr_index], self.heap_list[parent_index] = (
+                    self.heap_list[parent_index], self.heap_list[curr_index])
+            curr_index = parent_index
+            parent_index = curr_index // 2
+
+    def _perc_down(self, curr_index):
+        child_index = curr_index * 2
+        while child_index <= self.current_size:
+            minchild = self._min_child(child_index)
+            self.heap_list[minchild], self.heap_list[curr_index] = (
+                self.heap_list[curr_index], self.heap_list[minchild])
+            curr_index = minchild
+            child_index = curr_index * 2
+
+    def _min_child(self, first_child_i):
+        second_child_i = first_child_i + 1
+        if second_child_i > self.current_size:
+            return first_child_i
+        else:
+            if self.heap_list[second_child_i] < self.heap_list[first_child_i]:
+                return second_child_i
+            else:
+                return first_child_i
+
+    def get(self):
+        min_value = self.heap_list[1]
+        self.heap_list[1] = self.heap_list.pop()
+        self.current_size -= 1
+        self._perc_down(1)
+        return min_value
+
+    def build_heap(self, values):
+        new_values = list(values)
+        self.current_size = len(new_values)
+        curr_index = self.current_size // 2
+        self.heap_list = [0] + new_values
+        while curr_index > 0:
+            print(curr_index)
+            print(self)
+            self._perc_down(curr_index)
+            curr_index -= 1
+
+    def __str__(self):
+        index = 1
+        count = 2
+        retval = ''
+        while index < self.current_size:
+            retval += (str(self.heap_list[index:count]) + '\n')
+            index = count
+            count = index * 2
+        return retval
+
+
+if __name__ == '__main__':
+    heap = BinaryHeap()
+    #for i in [1,6,3,10,323,32,3,2,55,1200,15]:
+    #    heap.insert(i)
+    heap.build_heap([2, 30, 1, 4])
+    print(heap.heap_list)
+    print(heap)
+
+
+# list of properties of a heap, test each property individually
+# how do I think of examples - random examples, simple examples that are easy to follow
+# debugging: use the debugger, add comments for every line, think of the simplest possible test case
---
+++ basics/test_binary_heap.py
@@ -0,0 +1,114 @@
+import itertools
+import pytest
+import random as r
+
+from binary_heap import BinaryHeap
+
+
+heaps = {
+    'built empty': BinaryHeap([]),
+    'empty': BinaryHeap(),
+    'single item': BinaryHeap([1])
+    }
+
+unsorted_lists = list(itertools.permutations([1, 2, 3]))
+
+
+
+
+
+def test_empty_heap():
+    heap = BinaryHeap()
+    is_valid_heap(heap)
+
+
+@pytest.mark.skip
+def test_simple_heaps_build():
+    for permutation in unsorted_lists:
+        heap = BinaryHeap(permutation)
+        is_valid_heap(heap)
+
+
+def test_manually_constructed_heaps():
+    for permutation in unsorted_lists:
+        heap = BinaryHeap()
+        for item in permutation:
+            heap.insert(item)
+        is_valid_heap(heap)
+
+
+
+
+def test_valid_heaps():
+    heap = BinaryHeap()
+    heap.heap_list = [0, 1, 2, 3]
+    heap.current_size = 3
+    is_valid_heap(heap)
+    heap2 = BinaryHeap()
+    heap2.heap_list = [0, 1, 2, 3, 4]
+    heap2.current_size = 4
+    is_valid_heap(heap2)
+
+
+def test_perc_down():
+    assert True
+
+
+def test_perc_up():
+    assert True
+
+#def test_naive_build_heap():
+#    heap = BinaryHeap()
+#    heap.heap_list = [0, 1, 2, 3, 5, 23, 120]
+#    heap.current_size = 6
+#    is_valid_heap(heap)
+
+
+
+# helper functions here
+
+def is_valid_heap(heap):
+    n_single_child = 0
+    n_leafs = 0
+    for parent_index in range(1, heap.current_size + 1):
+        child1, child2 = get_child_indices(heap, parent_index)
+        if child1:
+            assert heap.heap_list[child1] >= heap.heap_list[parent_index]
+            if child2:
+                # do the stuff for two children
+                assert heap.heap_list[child2] >= heap.heap_list[parent_index]
+            else:
+                # do the stuff for one child
+                n_single_child += 1
+        else:
+            # do the stuff for no children
+            n_leafs += 1
+    assert n_single_child <= 1
+    nearest_power_of_two = heap.current_size // 2
+    assert n_leafs == len(heap.heap_list[nearest_power_of_two + 1:])
+
+
+def can_assert_true(thing):
+    assert True
+
+def can_assert_false(thing):
+    assert False
+
+def get_child_indices(heap, index):
+    child_1 = index * 2
+    child_2 = child_1 + 1
+    if child_1 <= heap.current_size:
+        if child_2 <= heap.current_size:
+            return (child_1, child_2)
+        else:
+            return (child_1, None)
+    else:
+        return (None, None)
+
+
+
+# I can either have a function that returns True or False if I have a valid heap
+# and then uses asserts to check whether various things are giving me valid heaps
+# OR
+# I can have a master test_if_heap function that checks all the ways a heap can fail
+# and I can programatically generate various heaps and see whether those pass
